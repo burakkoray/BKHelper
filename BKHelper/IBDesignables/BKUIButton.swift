a7bc2666e09ad6c5df30e5be6f6f83c70fdce8a0
Burak Koray Kose <burakkorayk@gmail.com>
2018-08-30 14:07:29 +0300

    init

---
+++ BKHelper/IBDesignables/BKUIButton.swift
@@ -0,0 +1,38 @@
+//
+//  BKUIButton.swift
+//  BKHelper
+//
+//  Created by Burak Koray Kose on 29.08.2018.
+//  Copyright © 2018 Burak Koray Kose. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class BKUIButton : UIButton{
+    
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            layer.cornerRadius = cornerRadius
+            layer.masksToBounds = cornerRadius > 0
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        didSet {
+            layer.borderWidth = borderWidth
+        }
+    }
+    @IBInspectable var borderColor: UIColor? = UIColor.clear {
+        didSet {
+            layer.borderColor = borderColor?.cgColor
+        }
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+}
